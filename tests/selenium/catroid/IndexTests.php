b0bfe47df204099a08f6564028ed1dad32f85b4b
Tobias Gritschacher <tobijat@gmail.com>
2011-04-08 11:07:55 +0200

    adjusted selenium IndexTest to do uploads with cURL

--- tests/selenium/catroid/IndexTests.php
+++ tests/selenium/catroid/IndexTests.php
@@ -43,85 +43,61 @@ class IndexTests extends PHPUnit_Framework_TestCase
   {
     $this->selenium->stop();
   }
-  
+
   public function ajaxWait($waitfor)
   {
     // Loop initialization.
     for ($second = 0; $second <=600;$second++) {
 
-     // If loop is reached 60 seconds then break the loop.
-     if ($second >= 600) break;
+      // If loop is reached 60 seconds then break the loop.
+      if ($second >= 600) break;
 
-     // Search for element "link=ajaxLink" and if available then break loop.
-     try 
-     {        
-       if (($this->selenium->isElementPresent($waitfor))&&(!($this->selenium->isTextPresent("loading..."))))
-       break; 
-     } catch (Exception $e) {}
-     sleep(1);
+      // Search for element "link=ajaxLink" and if available then break loop.
+      try
+      {
+        if (($this->selenium->isElementPresent($waitfor))&&(!($this->selenium->isTextPresent("loading..."))))
+        break;
+      } catch (Exception $e) {}
+      sleep(1);
     }
   }
-  
-   public function doUpload() {    
-     
 
-     
-     for($i=1; $i< 20; $i++)
-     {
-       $fileName = 'test.zip';
-       $testFile = dirname(__FILE__).'/testdata/'.$fileName;
-       $fileChecksum = md5_file($testFile);
-       $fileSize = filesize($testFile);
-       $fileType = 'application/x-zip-compressed';
-      
-       $formData = array('projectTitle'=>'unitTest'.$i, 'projectDescription'=>'unitTestDescription'.$i, 'fileChecksum'=>$fileChecksum);
-       $fileData = array('upload'=>array('name'=>$fileName, 'type'=>$fileType,
-                          'tmp_name'=>$testFile, 'error'=>0, 'size'=>$fileSize));
-       $serverData = array('REMOTE_ADDR'=>'127.0.0.1');
-       $insertId = $this->upload->doUpload($formData, $fileData, $serverData);
-       $filePath = CORE_BASE_PATH.PROJECTS_DIRECTORY.$insertId.PROJECTS_EXTENTION;
-      
-       //test qrcode image generation
-       $this->assertTrue(is_file(CORE_BASE_PATH.PROJECTS_QR_DIRECTORY.$insertId.PROJECTS_QR_EXTENTION));
-       $this->assertNotEquals(0, $insertId);
-       $this->assertTrue(is_file($filePath));
-       $this->assertEquals(200, $this->upload->statusCode);
-       $this->assertTrue($this->upload->projectId > 0);
-       $this->assertTrue($this->upload->fileChecksum != null);
-       $this->assertEquals(md5_file($testFile), $this->upload->fileChecksum);
-       $this->assertTrue(is_string($this->upload->answer));
-       array_push($this->insertIDArray, $insertId);
+  public function doUpload() {
+    for($i=1; $i< 20; $i++) {      
+      $jsonResponse = $this->uploadTestProject('unitTest'.$i, 'unitTestDescription'.$i);
+      $insertId = $jsonResponse->projectId;
+      array_push($this->insertIDArray, $insertId);
     }
-    
-  }    
+
+  }
 
   public function deleteUploadedProjects()
   {
-     foreach ($this->insertIDArray as $insertId)
-     {
-       $filePath = CORE_BASE_PATH.PROJECTS_DIRECTORY.$insertId.PROJECTS_EXTENTION;
-       // test deleting from database
-       $this->upload->removeProjectFromFilesystem($filePath);    
-       $this->assertFalse(is_file($filePath));
-       //test deleting from filesystem
-       $this->upload->removeProjectFromDatabase($insertId);
-       $query = "SELECT * FROM projects WHERE id='$insertId'";
-       $result = pg_query($query) or die('DB operation failed: ' . pg_last_error());
-       $this->assertEquals(0, pg_num_rows($result));
+    foreach ($this->insertIDArray as $insertId)
+    {
+      $filePath = CORE_BASE_PATH.PROJECTS_DIRECTORY.$insertId.PROJECTS_EXTENTION;
+      // test deleting from database
+      $this->upload->removeProjectFromFilesystem($filePath);
+      $this->assertFalse(is_file($filePath));
+      //test deleting from filesystem
+      $this->upload->removeProjectFromDatabase($insertId);
+      $query = "SELECT * FROM projects WHERE id='$insertId'";
+      $result = pg_query($query) or die('DB operation failed: ' . pg_last_error());
+      $this->assertEquals(0, pg_num_rows($result));
     }
   }
-  
+
   public function testIndexPage() {
     $this->selenium->open(TESTS_BASE_PATH);
     $this->selenium->waitForPageToLoad(10000);
-    
+
     //test page title
     $this->assertRegExp("/Catroid Website/", $this->selenium->getTitle());
-    
+
     // test catroid header text
     $this->assertTrue($this->selenium->isElementPresent("xpath=//img[@class='catroidLettering']"));
-    
-     // test logo link
+
+    // test logo link
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@class='webHeadLogo']/a"));
     $this->selenium->click("xpath=//div[@id='aIndexWebLogoLeft']/a");
     //$this->selenium->selectWindow("_blank");
@@ -132,60 +108,60 @@ class IndexTests extends PHPUnit_Framework_TestCase
     $this->assertTrue($this->selenium->isElementPresent("xpath=//a[@id='aIndexWebLogoMiddle']"));
     //$this->selenium->click("xpath=//div[@class='webHeadTitleName']/a");
     $this->selenium->click("xpath=//a[@id='aIndexWebLogoMiddle']");
-    
+
     $this->selenium->waitForPageToLoad(10000);
     $this->selenium->selectWindow("_blank");
-    
+
     //$this->selenium->waitForPageToLoad(10000);
     $this->assertTrue($this->selenium->isTextPresent("Catroid_0-4-3d.apk"));
     $this->assertTrue($this->selenium->isTextPresent("Paintroid_0.6.4b.apk"));
     $this->selenium->close();
     $this->selenium->selectWindow(null);
-    
+
     //test home link
     $this->selenium->click("xpath=//a[1]"); //clicks first link on page (should be the home link)
     $this->selenium->waitForPageToLoad(10000);
     $this->assertRegExp("/catroid\/index/", $this->selenium->getLocation());
-    
+
     //test links to details page
-    
+
     $this->ajaxWait("id=page0");
     $this->selenium->waitForCondition("", 2000); // loading...
-    $this->selenium->click("xpath=//a[@class='projectListDetailsLink']");    
-    $this->selenium->waitForPageToLoad(10000);    
+    $this->selenium->click("xpath=//a[@class='projectListDetailsLink']");
+    $this->selenium->waitForPageToLoad(10000);
     $this->assertRegExp("/catroid\/details/", $this->selenium->getLocation());
-    $this->selenium->goBack();    
+    $this->selenium->goBack();
     $this->ajaxWait("id=page0");
     $this->selenium->waitForPageToLoad(10000);
-    
-    
+
+
     $this->selenium->click("xpath=//a[@class='projectListDetailsLinkBold']");
     $this->selenium->waitForPageToLoad(10000);
-    
+
     $this->assertRegExp("/catroid\/details/", $this->selenium->getLocation());
-    
+
   }
-  
+
   public function testPageNavigation()
   {
     $this->doUpload();
     $this->selenium->open(TESTS_BASE_PATH);
     $this->selenium->waitForPageToLoad(10000);
-    
+
     //test page title
     $this->assertRegExp("/Catroid Website/", $this->selenium->getTitle());
     $this->assertFalse($this->selenium->isVisible("fewerProjects"));
     $this->assertTrue($this->selenium->isVisible("moreProjects"));
     $this->ajaxWait("id=page0");
     $this->selenium->waitForCondition("", 2000); // loading...
-    
+
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page0']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page1']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page2']"));
-    $this->selenium->click("moreProjects");    
+    $this->selenium->click("moreProjects");
     $this->ajaxWait("id=page2");
     $this->selenium->waitForCondition("", 2000); // loading...
-    
+
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page2']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page3']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page4']"));
@@ -194,43 +170,75 @@ class IndexTests extends PHPUnit_Framework_TestCase
     $this->selenium->refresh();
     $this->ajaxWait("id=page2");
     $this->selenium->waitForCondition("", 2000); // loading...
-    
+
     $this->selenium->waitForPageToLoad("10000");
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page2']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page3']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page4']"));
     $this->assertTrue($this->selenium->isVisible("fewerProjects"));
-            
+
     $this->selenium->click("fewerProjects");
     $this->ajaxWait("id=page0");
     $this->selenium->waitForCondition("", 2000); // loading...
-    
+
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page0']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page1']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page2']"));
     $this->assertFalse($this->selenium->isVisible("fewerProjects"));
-    
+
     // test header click
     $this->assertTrue($this->selenium->isVisible("moreProjects"));
-    $this->selenium->click("moreProjects");    
+    $this->selenium->click("moreProjects");
     $this->ajaxWait("id=page2");
     $this->selenium->waitForCondition("", 2000); // loading...
-    
+
     $this->selenium->click("aIndexWebLogoLeft");
     $this->ajaxWait("id=page0");
     $this->selenium->waitForCondition("", 2000); // loading...
-    
+
     $this->selenium->waitForPageToLoad("10000");
     $this->selenium->waitForCondition("", 2000); // necessary for loading delay
-    
+
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page0']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page1']"));
     $this->assertTrue($this->selenium->isElementPresent("xpath=//div[@id='page2']"));
     $this->assertFalse($this->selenium->isVisible("fewerProjects"));
-    
+
     $this->deleteUploadedProjects();
-  }   
-  
+  }
+
+  // upload a test project via cURL-request
+  private function uploadTestProject($title, $description = '')
+  {
+    $uploadTestFile = dirname(__FILE__);
+    if(strpos($uploadTestFile, '\\') >= 0) {
+      $uploadTestFile.= '\testdata\test.zip';
+    } else {
+      $uploadTestFile.= '/testdata/test.zip';
+    }
+
+    $uploadpath= TESTS_BASE_PATH.'catroid/upload/upload.json';
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
+    curl_setopt($ch, CURLOPT_URL, $uploadpath);
+    curl_setopt($ch, CURLOPT_POST, true);
+    $post = array(
+        "upload"=>"@$uploadTestFile",
+        "projectTitle"=>$title,
+    	"projectDescription"=>$description,
+        "fileChecksum"=>md5_file($uploadTestFile)
+    );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    $response = json_decode(curl_exec($ch));
+    $this->assertEquals(200, $response->statusCode);
+
+    return($response);
+  }
+
 }
 ?>
 
