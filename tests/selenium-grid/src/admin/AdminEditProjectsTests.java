a30d78f7dcdb3bbbce6a9d2393d02e207410458a
Christian Hofer <chrisss404@gmail.com>
2011-05-11 14:50:16 +0200

    selenium grit initial commit

---
+++ tests/selenium-grid/src/admin/AdminEditProjectsTests.java
@@ -0,0 +1,39 @@
+package at.tugraz.ist.catroweb.admin.editProjects;
+
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.closeSeleniumSession;
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.startSeleniumSession;
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+/**
+ * Base class for all tests in Selenium Grid Java examples.
+ */
+public class AdminEditProjectsTests {
+
+    public static final String TIMEOUT = "120000";
+
+    @BeforeMethod(groups = {"default", "catroid"}, alwaysRun = true)
+    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
+    protected void startSession(String seleniumHost, int seleniumPort, String browser, String webSite) {
+        startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
+        session().setTimeout(TIMEOUT);
+    }
+
+    @AfterMethod(groups = {"default", "catroid"}, alwaysRun = true)
+    protected void closeSession() {
+        closeSeleniumSession();
+    }
+
+    @Test(groups = {"catroid", "firefox", "default"}, description = "Header Buttons Index")
+    public void headerButtonsIndex() throws Throwable {
+        session().setSpeed("1000");
+        session().open("/");
+        session().waitForPageToLoad(TIMEOUT);
+    }
+}
+
---
+++ tests/selenium-grid/src/catroid/LicenceTests.java
@@ -0,0 +1,44 @@
+package at.tugraz.ist.catroweb.catroid.licence;
+
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.closeSeleniumSession;
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.session;
+import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.startSeleniumSession;
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+/**
+ * Base class for all tests in Selenium Grid Java examples.
+ */
+public class LicenceTests {
+
+    public static final String TIMEOUT = "120000";
+
+    @BeforeMethod(groups = {"default", "licence"}, alwaysRun = true)
+    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
+    protected void startSession(String seleniumHost, int seleniumPort, String browser, String webSite) {
+        startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
+        session().setTimeout(TIMEOUT);
+    }
+
+    @AfterMethod(groups = {"default", "licence"}, alwaysRun = true)
+    protected void closeSession() {
+        closeSeleniumSession();
+    }
+
+    @Test(groups = {"licence", "firefox", "default"}, description = "Imprint")
+    public void imprint() throws Throwable {
+        session().setSpeed("1000");
+        session().open("/");
+        session().waitForPageToLoad(TIMEOUT);
+        session().click("xpath=//a[@class='license'][5]");
+        session().waitForPageToLoad(TIMEOUT);
+        assertTrue(session().isTextPresent(("Contact us")));
+        session().isElementPresent("xpath=//p[@class='licenseText']/a");
+        session().goBack();
+        session().waitForPageToLoad(TIMEOUT);
+    }
+}
