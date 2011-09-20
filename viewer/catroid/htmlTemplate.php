86ec0dd4fcf8f97d1e8e865c6daa7f3d56597c42
Christian Hofer <chrisss404@gmail.com>
2011-09-20 12:34:24 +0200

    removed big margin from qrCodeGenerator; removed unused files (viewer/test); updated build file for catroidtestserver.ist.tugraz.at; updated help page

--- viewer/catroid/htmlTemplate.php
+++ viewer/catroid/htmlTemplate.php
@@ -49,7 +49,7 @@
 <?php while($js = $this->getJs()) {?>
   <script type="text/javascript" src="<?php echo BASE_PATH?>include/script/<?php echo $js.'?'.VERSION?>"></script>
 <?php }?>
-  <link rel="icon" href="<?php echo BASE_PATH?>images/logo/favicon.png" type="image/png" />
+  <link rel="icon" href="<?php echo BASE_PATH?>images/logo/favicon.png<?php echo '?'.VERSION?>" type="image/png" />
 </head>
 
 <body>
