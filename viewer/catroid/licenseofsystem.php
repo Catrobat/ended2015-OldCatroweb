5b3cb2d432762713892469f407ae804031d77451
Alexander Gütler <alexander.guetler@gmail.com>
2011-10-29 22:55:57 +0200

    changed all license text in source-code and added additional license pages

---
+++ viewer/catroid/licenseofsystem.php
@@ -0,0 +1,91 @@
+<?php
+/*    Catroid: An on-device graphical programming language for Android devices
+ *    Copyright (C) 2010-2011 The Catroid Team
+ *    (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    An additional term exception under section 7 of the GNU Affero
+ *    General Public License, version 3, is available at
+ *    http://www.catroid.org/catroid/licenseadditionalterm
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+    <div class="webMainMiddle">
+      <div class="blueBoxMain">
+        <div class="webMainContent">
+          <div class="webMainContentTitle">Licenses of the Catroid system</div>
+          <div class="licenseMain">            	
+            <div class ="whiteBoxMain">
+              <div class="licenseText">
+                <p class="licenseText">
+The following licenses, terms, and conditions apply to your use of the Catroid system:<br/>
+<ul>
+<li>All parts of our own software related to the Catroid system, e.g., the Catroid Integrated Development Environment (with all associated parts such as the Tutorial), the Catroid Interpreter, Paintroid, the software we wrote to run the Catroid websites and other Internet resources, and all our own source code related to the Catroid system, are <a href="http://www.gnu.org/philosophy/free-sw.html" target="_blank">free software</a>: you can redistribute them and/or modify them under the terms of the GNU Affero General Public License as published by the <a href="http://www.fsf.org" target="_blank">Free Software Foundation</a>, either <a href="/catroid/agpl3standalone">version 3 of the License</a>, or (at your option) any <a href="http://www.gnu.org/licenses/agpl.html" target="_blank">later version when it becomes available</a>.<br/><br/>
+<li>The names and logos of Catroid, Paintroid, and Kittyroid, are copyrighted by and trademarks of the Catroid project and cannot be used in derivative work. You are however allowed to use the names and logos of Catroid, Paintroid, and Kittyroid in non-derivative work such as, e.g., books, brochures, or descriptive web pages. We do ask to include a note that either says <i>&quot;Catroid is developed by the Catroid project <a href="http://www.catroid.org/">http://www.catroid.org</a>&quot;</i> or <i>&quot;Catroid makes it easy to program your own interactive stories, games, and animations --- and share your creations on the web. As you create and share projects with Catroid, you learn to think creatively, reason systematically, and work collaboratively. Catroid is developed by the Catroid project. It is available as free open source software from <a href="http://www.catroid.org">http://www.catroid.org</a>&quot;</i> (replace Catroid by, or add Paintroid and/or Kittyroid, depending on which you refer to).<br/><br/>
+<li>For the parts of the Catroid system necessary for running a Catroid project, e.g., the Catroid Interpreter, the GNU Affero General Public License is supplemented by an additional permission under section 7 of <a href="/catroid/agpl3standalone">version 3 of the license</a>. This exception allows you to convey a runtime version of a Catroid project, for which you otherwise legally hold the necessary rights, in combination with the parts of the Catroid system necessary for running that Catroid project, under terms of <strong>*your*</strong> choice, consistent with the licensing of the independent parts of the corresponding Catroid project. Please note that this is only possible either when you are the sole creator of the project including all its parts such as graphics, texts, sounds, etc, or in case you licensed the parts of other projects on which your project is based upon, including any additional material and/or possible trademarks etc, in such a way that you are allowed to convey the resulting software under your chosen terms in consistence with these third-party materials. This additional permission is similar to the <a href0"http://www.gnu.org/licenses/gcc-exception.html" target="_blank">additional permission for GCC</a> (see the corresponding <a href="http://www.gnu.org/licenses/gcc-exception-faq.html" target="_blank">Rationale and FAQ page for the exception for GCC</a> for more information). If the work thus created has an interactive user interface, it must display a note in a screen shown when the user presses an &quot;About&quot; menu button or a similar way to convey such a message that says &quot;This work was created using programs developed by the Catroid project. Catroid makes it easy to program your own interactive stories, games, and animations --- and share your creations on the web. As you create and share projects with Catroid, you learn to think creatively, reason systematically, and work collaboratively. Catroid is available as free open source software from <a href="http://www.catroid.org/">http://www.catroid.org</a> and is distributed by the Catroid project under the GNU Affero GNU Affero General Public License as published by the <a href="http://www.fsf.org" target="_blank">Free Software Foundation</a>, either <a href="/catroid/agpl3standalone">version 3 of the License</a>, or (at your option) any <a href="http://www.gnu.org/licenses/agpl.html" target="_blank">later version when it becomes available</a>. For the parts of the Catroid system necessary for running a Catroid project, e.g., the Catroid Interpreter, the GNU Affero General Public License is supplemented by an additional permission under section 7 of <a href="/catroid/agpl3standalone">version 3 of the license</a>. This exception allows to convey a runtime version of a Catroid project, for which you otherwise legally hold the necessary rights, in combination with the parts of the Catroid system necessary for running that Catroid project, under terms of your choice, consistent with the licensing of the independent parts of the corresponding Catroid project.&quot;. The same notice must also be added by you as text in or along any documents you distribute together with your runtime version of the project. Please consult our <a href="/catroid/licenseadditionalterm">Catroid License Additional Term page</a> for the precise legal formulation of the additional permission under section 7 of version 3 of the GNU Affero GNU Affero General Public License. For clarification, note that this additional permission does <strong>*not*</strong> apply to projects you upload to Catroid&apos;s website. Please consult <a href="/catroid/terms">http://www.catroid.org/catroid/terms</a> for the terms and <a href="/catroid/licenseofuploadedprojects">http://www.catroid.org/catroid/licenseofuploadedprojects</a> for the precise legal terms and conditions that apply to a project you upload to Catroid's website.<br/><br/>
+<li>All non-software artifacts of the Catroid system created by us, such as, e.g., text, graphics, photos, sounds, music, videos, audiovisual combinations, interactive features, and other materials are available under the <a href="/catroid/ccbysa3">Creative Commons Attribution-ShareAlike 3.0 License</a>, the only exceptions being the names and logos of Catroid, Paintroid, and Kittyroid, which are trademarks of the Catroid project and cannot be used in derivative work. See <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">http://creativecommons.org/licenses/by-sa/3.0/</a> for a human-readable summary of the  Creative Commons Attribution-ShareAlike 3.0 License.<br/>
+</ul>
+
+The source code of the Catroid project additionally contains third party code as follows:<br/>
+<ul>
+<li><p>XStream: <a href="http://xstream.codehaus.org" target="_blank">&lt;http://xstream.codehaus.org&gt;</a><br/>
+License information: <a href="http://xstream.codehaus.org/license.html" target="_blank">&lt;http://xstream.codehaus.org/license.html&gt;</a><br/>
+Copyright &copy; 2003-2006, Joe Walnes<br/>
+Copyright &copy; 2006-2009, XStream Committers<br/>
+All rights reserved.<br/>
+<br/>
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+<br/><br/>
+Redistributions of source code must retain the above copyright notice, this list of
+conditions and the following disclaimer. Redistributions in binary form must reproduce
+the above copyright notice, this list of conditions and the following disclaimer in
+the documentation and/or other materials provided with the distribution.
+<br/><br/>
+Neither the name of XStream nor the names of its contributors may be used to endorse
+or promote products derived from this software without specific prior written
+permission.
+<br/><br/>
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &quot;AS IS&quot; AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+<br/>
+</p>
+<li><p>
+libGDX <a href="http://libgdx.badlogicgames.com" target="_blank">&lt;http://libgdx.badlogicgames.com&gt;</a><br/>
+License information: Apache 2 License <a href="http://libgdx.badlogicgames.com/license.php" target="_blank">&lt;http://libgdx.badlogicgames.com/license.php&gt;</a><br/>
+Copyright &copy; 2011 See <a href="http://code.google.com/p/libgdx/people/list" target="_blank">&lt;http://code.google.com/p/libgdx/people/list&gt;</a>
+</p>
+</ul>
+<br/>
+Check back here from time-to-time, because we are always trying to improve our guidelines.<br/>
+<br/> 
+Please mail any suggestions regarding these licenses of uploaded Catroid projects to <a href="mailto:webmaster@catroid.org?subject=Licenses%20of%20the%20Catroid%20system">webmaster@catroid.org</a>
+<br/><br/>
+Dated: 27 October 2011<br/>
+Copyright &copy; 2011 Catroid Team <a href="http://catroid.org">&lt;http://catroid.org&gt;</a><br/>
+								</p>
+              </div> <!-- License Text -->
+            </div> <!--  White Box -->            	
+          </div> <!--  license Main -->
+        </div> <!-- mainContent close //-->
+      </div> <!-- blueBoxMain close //-->
+    </div>
---
+++ viewer/catroid/licenseofuploadedprojects.php
@@ -0,0 +1,52 @@
+<?php
+/*    Catroid: An on-device graphical programming language for Android devices
+ *    Copyright (C) 2010-2011 The Catroid Team
+ *    (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    An additional term exception under section 7 of the GNU Affero
+ *    General Public License, version 3, is available at
+ *    http://www.catroid.org/catroid/licenseadditionalterm
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+    <div class="webMainMiddle">
+      <div class="blueBoxMain">
+        <div class="webMainContent">
+          <div class="webMainContentTitle">Licenses of uploaded Catroid projects</div>
+          <div class="licenseMain">            	
+            <div class ="whiteBoxMain">
+              <div class="licenseText">
+                <p class="licenseText">
+By uploading a project to Catroid's website, you accept and agree to put this work under the following licenses and the Catroid terms of service:
+<ul>
+<li>The uploaded project as a whole is made available under the terms of the GNU Affero General Public License as published by the <a href="http://www.fsf.org/" target="_blank">Free Software Foundation</a>, either <a href="/catroid/agpl3standalone">version 3 of the License</a>, or (at the option of the person accepting the license) any <a href="http://www.gnu.org/licenses/agpl.html" target="_blank">later version when it becomes available</a>.<br/><br/>
+<li>All non-software artifacts of the uploaded project, such as, e.g., text, graphics, photos, sounds, music, videos, audiovisual combinations, interactive features, and other materials are additionally made available under the <a href="/catroid/ccbysa3">Creative Commons Attribution-ShareAlike 3.0 License</a>. See <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">http://creativecommons.org/licenses/by-sa/3.0/</a> for a human-readable summary of this license.<br/><br/>
+<li>You accept and agree to our <a href="/catroid/termsofservice">http://www.catroid.org/catroid/termsofservice</a>.<br/><br/>
+</ul>
+<br/>
+Check back here from time-to-time, because we are always trying to improve our guidelines. <br/>
+<br/>
+Please mail any suggestions regarding these licenses of uploaded Catroid projects to <a href="mailto:webmaster@catroid.org?subject=Licenses%20of%20uploaded%20Catroid%20projects">webmaster@catroid.org</a>. 
+<br/>
+<br/>
+Dated: 26 October 2011<br/>
+Copyright © 2011 Catroid Team <a href="http://catroid.org">&lt;http://catroid.org&gt;</a><br/>
+								</p>
+              </div> <!-- License Text -->
+            </div> <!--  White Box -->            	
+          </div> <!--  license Main -->
+        </div> <!-- mainContent close //-->
+      </div> <!-- blueBoxMain close //-->
+    </div>
