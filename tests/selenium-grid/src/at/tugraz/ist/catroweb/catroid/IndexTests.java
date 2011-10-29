8e4309db105a66817775e636549c094def8fd757
Roman Mauhart <mauhart@gmail.com>
2011-10-29 15:25:18 +0200

    6.143W Infobox text, screenshot and test added

--- tests/selenium-grid/src/at/tugraz/ist/catroweb/catroid/IndexTests.java
+++ tests/selenium-grid/src/at/tugraz/ist/catroweb/catroid/IndexTests.java
@@ -29,6 +29,21 @@ import at.tugraz.ist.catroweb.common.*;
 @Test(groups = { "catroid", "IndexTests" })
 public class IndexTests extends BaseTest {
 
+  @Test(groups = { "visibility" }, description = "information box, description and screenshot")
+  public void infoBox() throws Throwable {
+    openLocation();
+    ajaxWait();
+
+    assertTrue(isVisible(By.id("infobox")));
+    assertTrue(isTextPresent("Visual Programming Language");
+    assertTrue(isTextPresent("Catroid is an on-device graphical programming language for Android devices that is inspired by the Scratch programming language for PCs, developed by the Lifelong Kindergarten Group at the MIT Media Lab. It is the aim of the Catroid project to facilitate the learning of programming skills among children and users of all ages.");
+    assertTrue(isElementPresent(By.id("infoboxScreenshot")));
+    driver().findElement(By.id("infoboxLink")).click();
+    driver().findElement(By.id("infoboxClose")).click();
+    ajaxWait();
+    assertFalse(isVisible(By.id("infobox")));
+  }
+  
   @Test(groups = { "visibility" }, description = "location tests")
   public void location() throws Throwable {
     String longPageNr = "99999999";
@@ -67,7 +82,6 @@ public class IndexTests extends BaseTest {
       // random string instead of page nr should redirect to first page
       openLocation("catroid/search/?q=test&p=" + CommonData.getRandomShortString(10));
       ajaxWait();
-      assertTrue(isTextPresent(CommonStrings.SEARCH_PROJECTS_PAGE_TITLE));
       assertRegExp(".*/catroid/search/[?]q=test[&]p=1.*", driver().getCurrentUrl());
 
       openLocation("catroid/profile");
