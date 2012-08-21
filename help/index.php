0909239a294e4747b95a203348b629ae18047d46
Christian Hofer <chrisss404@gmail.com>
2012-08-21 15:28:30 +0200

    added update-selenium target to help file; added link to VirtualBox image link to help file

--- help/index.php
+++ help/index.php
@@ -33,6 +33,10 @@ define('REAL_BASE_PATH',str_replace('help/', '', BASE_PATH));
 		<div id="antcommands">
 			<h1>ant cheatsheet</h1>
 			<pre>
+--- update selenium libs
+
+    ant update-selenium
+
 --- local tests
 
     ant run-phpunit-tests		 
@@ -75,6 +79,9 @@ define('REAL_BASE_PATH',str_replace('help/', '', BASE_PATH));
       </pre>
 		</div>
 		<div id="links">
+			<h1>Developement Environment</h1>
+      <a href="http://catroidtestserver.ist.tugraz.at/development/current-webbox.ova" target="_blank">VirutalBox Image</a> - Contains the development environment.<br/>
+			
 			<h1>Localhost</h1>
 			<a href="http://<?php echo $_SERVER['SERVER_NAME']?>:4444/grid/console" target="_blank">Grid	Console</a> - Selenium-Grid Server Status<br/> 
 			<a href="<?php echo REAL_BASE_PATH;?>tests/selenium-grid/target/reports/" target="_blank">Test	Reports</a> - Test results of last testrun<br/> 
@@ -102,8 +109,9 @@ define('REAL_BASE_PATH',str_replace('help/', '', BASE_PATH));
       <a href="http://catroidtest.ist.tugraz.at/phppgadmin/" target="_blank">phpPgAdmin</a>* -  WebBased SQL administration tool<br/>
       <a href="https://selenium.googlecode.com/svn/trunk/java/CHANGELOG" target="_blank">Selenium changelog</a> - Most recent changes<br/>
       
-      <p><strong>* Note:</strong> only accessible with an TUGraz IP address</p>	
+      <p><strong>* Note:</strong> only accessible with a TUGraz IP address</p>	
 		</div>
+		<div style="clear: both;"></div>
 	</div>
 </body>
 </html>
