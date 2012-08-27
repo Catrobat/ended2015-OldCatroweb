8ba3872aaf411acfa4f97d2fd18bcabb3cdc2acd
Roman Mauhart <mauhart@gmail.com>
2012-08-27 10:25:59 +0200

    fixed db-update script: missing language plpgsql

--- sql/catroweb/updates/20_create_blocked_ips_temporary.sql
+++ sql/catroweb/updates/20_create_blocked_ips_temporary.sql
@@ -1,3 +1,22 @@
+CREATE OR REPLACE FUNCTION make_plpgsql()
+RETURNS VOID
+LANGUAGE SQL
+AS $$
+CREATE LANGUAGE plpgsql;
+$$;
+ 
+SELECT
+    CASE
+    WHEN EXISTS(
+        SELECT 1
+        FROM pg_catalog.pg_language
+        WHERE lanname='plpgsql'
+    )
+    THEN NULL
+    ELSE make_plpgsql() END;
+ 
+DROP FUNCTION make_plpgsql();
+
 CREATE TABLE blocked_ips_temporary
 (
   id serial NOT NULL PRIMARY KEY,
