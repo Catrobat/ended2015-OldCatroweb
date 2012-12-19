2242abe5ae99b5d0a25bec86c5f09220d71dd0fa
Christian Hofer <chrisss404@gmail.com>
2012-12-19 15:23:23 +0100

    added deploy script

---
+++ scripts/deploy.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+'''   
+ *    Catroid: An on-device graphical programming language for Android devices
+ *    Copyright (C) 2010-2012 The Catroid Team
+ *    (<http://code.google.com/p/catroid/wiki/Credits>)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import fileinput, glob, os, shutil, sys, paramiko
+from datetime import date, datetime, timedelta
+from release import Release
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class Deploy:
+	basePath					= os.getcwd()
+	today							= date.today().strftime("%Y%m%d")
+	buildDir					= os.path.join(basePath, 'build')
+	
+	ssh								= None
+	sftp							= None
+	remoteDir					= '/var/www/catroid'
+	
+	#--------------------------------------------------------------------------------------------------------------------
+	def __init__(self, server, user, password, port=22):
+		self.ssh = paramiko.SSHClient()
+		self.ssh.load_system_host_keys()
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		self.ssh.connect(server, port, user, password)
+		self.sftp = paramiko.SFTPClient.from_transport(self.ssh.get_transport())
+
+	#--------------------------------------------------------------------------------------------------------------------
+	def remoteCommand(self, command):
+		(stdin, stdout, stderr) = self.ssh.exec_command('cd ' + self.remoteDir + '; ' + command)
+		error = stderr.readlines()
+		if len(error) > 0:
+			print '** ERROR ***********************************************************************'
+			print error
+			return stdout.readlines() + error
+		return stdout.readlines()
+
+	#--------------------------------------------------------------------------------------------------------------------
+	def formatSize(self, number):
+		for unit in ['B  ', 'KiB', 'MiB', 'GiB']:
+			if number < 1024.0:
+				return "%6.1f %s" % (number, unit)
+			number /= 1024.0
+		return "%6.1f %s" % (number, 'TiB')
+
+	#--------------------------------------------------------------------------------------------------------------------
+	def getSize(self, path = '.'):
+		totalSize = 0
+		for file in os.listdir(path):
+			fileIterator = os.path.join(path, file)
+			if os.path.isfile(fileIterator):
+				totalSize += os.path.getsize(fileIterator)
+		return totalSize
+
+	#--------------------------------------------------------------------------------------------------------------------
+	def upload(self, localPath, remotePath):
+		totalSize = 0
+		startTime = datetime.now()
+		
+		os.chdir(os.path.dirname(localPath))
+		for (path, dirs, files) in os.walk(os.path.basename(localPath)):
+			directorySize = self.getSize(path)
+			print self.formatSize(directorySize) + ' - - ' + os.path.join(remotePath, path)
+			totalSize += directorySize
+
+			try:
+				self.sftp.mkdir(os.path.join(remotePath, path))
+			except:
+				pass
+			for file in files:
+				self.sftp.put(os.path.join(path, file), os.path.join(remotePath, path, file))
+		
+		print 'copied ' +  self.formatSize(totalSize) + ' in ' + str((datetime.now() - startTime).seconds) + ' seconds.'
+		print ''
+
+	#--------------------------------------------------------------------------------------------------------------------
+	def moveFilesIntoPlace(self, localPath, release):
+		os.chdir(os.path.dirname(localPath))
+		for entry in os.listdir(os.path.basename(localPath)):
+			self.remoteCommand('rm -rf ' + entry)
+
+		self.remoteCommand('mv ' + release + '/* .')
+		self.remoteCommand('rm -rf ' + release)
+		
+		if 'No such file' in str(self.remoteCommand('ls passwords.php')):
+			print 'Warning: passwords.php is missing, please add one.'
+
+		self.remoteCommand('mkdir -m 0777 -p cache')
+		self.remoteCommand('mkdir -m 0777 -p resources/catroid')
+		self.remoteCommand('mkdir -m 0777 -p resources/projects')
+		self.remoteCommand('mkdir -m 0777 -p resources/qrcodes')
+		self.remoteCommand('mkdir -m 0777 -p resources/thumbnails')
+		
+	#--------------------------------------------------------------------------------------------------------------------
+	def run(self, release=today):
+		if not os.path.isdir(os.path.join(self.buildDir, release)):
+			if not os.path.isdir(os.path.join(self.buildDir, self.today)):
+				Release().create()
+		
+		self.upload(os.path.join(self.buildDir, release), self.remoteDir)
+		self.moveFilesIntoPlace(os.path.join(self.buildDir, release), release)
+		print 'hi' + release
+		
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## command handler
+if __name__ == '__main__':
+	Deploy('hostname', 'user', 'pwd').run()
--- scripts/release.py
+++ scripts/release.py
@@ -19,42 +19,50 @@
 '''
 
 import fileinput, glob, os, shutil, sys
-
-## TODO add init script (should check if jquery exists. 
+from datetime import date
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Release:
 	basePath					= os.getcwd()
+	today							= date.today().strftime("%Y%m%d")
 	buildDir					= os.path.join(basePath, 'build')
-	resourceDir				= os.path.join(basePath, 'resources')
+	releaseDir				= os.path.join(buildDir, today)
 
 	#--------------------------------------------------------------------------------------------------------------------	
 	def copyFiles(self):
+		
 		## clean build destination if it exists
-		if os.path.isdir(self.buildDir):
-			shutil.rmtree(self.buildDir)
-		os.mkdir(self.buildDir)
+		if not os.path.isdir(self.buildDir):
+			os.mkdir(self.buildDir)
+		if os.path.isdir(self.releaseDir):
+			shutil.rmtree(self.releaseDir)
+		os.mkdir(self.releaseDir)
 		
 		## copy files
-		shutil.copy(os.path.join(self.basePath, 'commonFunctions.php'), self.buildDir)
-		shutil.copy(os.path.join(self.basePath, 'config.php'), self.buildDir)
-		shutil.copy(os.path.join(self.basePath, 'index.php'), self.buildDir)
-		shutil.copy(os.path.join(self.basePath, 'passwords.php'), self.buildDir)
-		shutil.copy(os.path.join(self.basePath, 'robots.txt'), self.buildDir)
-		shutil.copy(os.path.join(self.basePath, 'statusCodes.php'), self.buildDir)
-		shutil.copy(os.path.join(self.basePath, '.htaccess'), self.buildDir)
+		shutil.copy(os.path.join(self.basePath, 'commonFunctions.php'), self.releaseDir)
+		shutil.copy(os.path.join(self.basePath, 'config.php'), self.releaseDir)
+		shutil.copy(os.path.join(self.basePath, 'ga.php'), self.releaseDir)
+		shutil.copy(os.path.join(self.basePath, 'index.php'), self.releaseDir)
+		shutil.copy(os.path.join(self.basePath, 'robots.txt'), self.releaseDir)
+		shutil.copy(os.path.join(self.basePath, 'statusCodes.php'), self.releaseDir)
+		shutil.copy(os.path.join(self.basePath, '.htaccess'), self.releaseDir)
 
 		## copy dirs
-		shutil.copytree(os.path.join(self.basePath, 'addons'), os.path.join(self.buildDir, 'addons'))
-		shutil.copytree(os.path.join(self.basePath, 'classes'), os.path.join(self.buildDir, 'classes'))
-		shutil.copytree(os.path.join(self.basePath, 'images'), os.path.join(self.buildDir, 'images'))
-		shutil.copytree(os.path.join(self.basePath, 'include'), os.path.join(self.buildDir, 'include'))
-		shutil.copytree(os.path.join(self.basePath, 'modules'), os.path.join(self.buildDir, 'modules'))
-		shutil.copytree(os.path.join(self.basePath, 'sql'), os.path.join(self.buildDir, 'sql'))
-		shutil.copytree(os.path.join(self.basePath, 'viewer'), os.path.join(self.buildDir, 'viewer'))
+		shutil.copytree(os.path.join(self.basePath, 'addons'), os.path.join(self.releaseDir, 'addons'))
+		shutil.copytree(os.path.join(self.basePath, 'classes'), os.path.join(self.releaseDir, 'classes'))
+		shutil.copytree(os.path.join(self.basePath, 'images'), os.path.join(self.releaseDir, 'images'))
+		shutil.copytree(os.path.join(self.basePath, 'include'), os.path.join(self.releaseDir, 'include'))
+		shutil.copytree(os.path.join(self.basePath, 'modules'), os.path.join(self.releaseDir, 'modules'))
+		shutil.copytree(os.path.join(self.basePath, 'sql'), os.path.join(self.releaseDir, 'sql'))
+		shutil.copytree(os.path.join(self.basePath, 'tools'), os.path.join(self.releaseDir, 'tools'))
+		shutil.copytree(os.path.join(self.basePath, 'viewer'), os.path.join(self.releaseDir, 'viewer'))
+
+	#--------------------------------------------------------------------------------------------------------------------	
+	def create(self):
+		self.copyFiles()
 		
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## command handler
 if __name__ == '__main__':
-	Release().copyFiles()
+	Release().create()
