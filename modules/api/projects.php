2ef816be313009a5c26d250abf756d428159a866
Patrick Trummer <patricktrummer88@gmail.com>
2013-08-14 12:45:42 +0200

    fix bug in UserSearch

--- modules/api/projects.php
+++ modules/api/projects.php
@@ -178,7 +178,7 @@ class projects extends CoreAuthenticationNone {
     }
   
   
-    $searchTerms = explode(" ", $query);
+    $searchTerms = array("$query");
     foreach($searchTerms as $term) {
       if(strlen($term) > 0) {
         $searchQuery .= (($searchQuery == "") ? " AND (" : " OR " );
