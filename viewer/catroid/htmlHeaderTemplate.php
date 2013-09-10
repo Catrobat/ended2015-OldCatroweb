f70971a13f2d885206a77a3b13f98fa135cad5fb
Gregor Sitter <g.sitter@student.tugraz.at>
2013-09-10 13:59:40 +0200

    updated avatar border for mobile versions

--- viewer/catroid/htmlHeaderTemplate.php
+++ viewer/catroid/htmlHeaderTemplate.php
@@ -65,7 +65,7 @@
             <div class="marginSpacer"></div>
 
             <div id="mobileMenuButton">
-              <button class="img-avatar"<?php echo ($this->module->session->userLogin_userAvatar) ? ' style="background-size:cover; background-position:center; background-repeat: norepeat; background-image:url(' . $this->module->session->userLogin_userAvatar . ');"' : ''; ?>></button>
+              <button class="img-avatar"<?php echo ($this->module->session->userLogin_userAvatar) ? ' style="background-size:cover; background-position:center; background-repeat: norepeat; background-image:url(' . $this->module->session->userLogin_userAvatar . ');outline:1px solid #FFFFFF;"' : ''; ?>></button>
             </div>
             
             <div class="marginSpacer"></div>
