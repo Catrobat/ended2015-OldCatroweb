c917da4025883a1cae2b64fd8a2e46d780aad905
Marko Burazer <mburazer@student.tugraz.at>
2013-11-01 20:05:24 +0100

    fixed typo

--- tests/selenium-grid/src/at/tugraz/ist/catroweb/admin/AdminTests.java
+++ tests/selenium-grid/src/at/tugraz/ist/catroweb/admin/AdminTests.java
@@ -308,7 +308,7 @@ public class AdminTests extends BaseTest {
       assertTrue(isTextPresent("Unapproved projects: "));
       driver().findElement(By.id("aAdminApprovedProjects")).click();
       ajaxWait();
-      assertTrue(isTextPresent("Administration Tools - List of inappropriate projects"));
+      assertTrue(isTextPresent("Administration Tools - List of unapproved projects"));
       clickOkOnNextConfirmationBox();
       driver().findElement(By.xpath("//*[@id='projectTableId']/tbody/tr[2]/td[6]/form/input[3]")).click();
       ajaxWait();
