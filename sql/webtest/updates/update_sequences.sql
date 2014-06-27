ae1dc0e0818acfc331400cf602f5ded3c6e110fd
Marko Burazer <mburazer@student.tugraz.at>
2014-06-27 17:49:39 +0200

    add sql files to webtest directory

--- sql/webtest/updates/update_sequences.sql
+++ sql/webtest/updates/update_sequences.sql
@@ -3,3 +3,4 @@ SELECT setval('projects_id_seq', (SELECT MAX(id) FROM projects) + 1);
 SELECT setval('flagged_projects_id_seq', (SELECT MAX(id) FROM flagged_projects) + 1);
 SELECT setval('wordlist_id_seq', (SELECT MAX(id) FROM wordlist) + 1);
 SELECT setval('unapproved_words_in_projects_id_seq', (SELECT MAX(id) FROM unapproved_words_in_projects) + 1);
+SELECT setval('intern_tagging_id_seq', (SELECT MAX(id) FROM intern_tagging) + 1);
