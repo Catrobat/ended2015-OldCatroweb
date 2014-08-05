0a8e39946b98603934dcabd8f8d6677022d5f3c5
Sebastian Krell <skrell@gmx.at>
2014-08-05 20:38:53 +0200

    repaired m4a-handling

--- classes/CorePresenter_m4a.php
+++ classes/CorePresenter_m4a.php
@@ -22,7 +22,7 @@
   * along with this program. If not, see <http://www.gnu.org/licenses/>.
   */
 
-class CorePresenter_mp3 extends CorePresenterCommon {
+class CorePresenter_m4a extends CorePresenterCommon {
   public function __construct(CoreModule $module) {
     parent::__construct($module);
   }
